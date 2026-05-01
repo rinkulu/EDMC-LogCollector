acc8b0c13ee7c184ee9ed58242f013e4f8563ab2
rinkulu <rinkulukat@gmail.com>
2026-05-02 00:37:14 +0300

    add pack_release script

---
+++ pack_release.py
@@ -0,0 +1,13 @@
+import zipfile
+from datetime import datetime, UTC
+
+files = ['load.py', 'L10n/en.strings', 'L10n/ru.strings']
+now = datetime.now(UTC)
+fixed_datetime = (now.year, now.month, now.day, now.hour, now.minute, now.second)
+
+with zipfile.ZipFile('EDMC-LogCollector.zip', 'w', compression=zipfile.ZIP_DEFLATED) as zf:
+    for file_path in files:
+        zinfo = zipfile.ZipInfo(filename=file_path, date_time=fixed_datetime)
+        zinfo.compress_type = zipfile.ZIP_DEFLATED
+        with open(file_path, 'rb') as f:
+            zf.writestr(zinfo, f.read())
